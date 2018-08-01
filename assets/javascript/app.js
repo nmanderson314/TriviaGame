c4d423e13914e825fa5612865c6199580a9d516e
Natalie A <nmanderson314@gmail.com>
2018-07-31 23:50:06 -0400

    buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,63 @@
+//show one question at a time
+//add a timeout to the question
+//question changes if a)user answers correctly or b) timer runs out
+//if correct answer - show answer was correct
+//if the user is wrong or runs out of time, show that they were wrong and display the correct answer
+//final screen: show score and 
+
+//billy eichner's Immigrant or "Real" American
+
+//real american: ted bundy, unibomber, casey anthony, 
+//immigrants: nikola tesla, lupita nyongo, ryan reynolds, ALEJANDRO GONZÁLEZ IÑÁRRITU 
+
+
+
+//!! LINK OUT TO CHARITY SITES
+//link to clip https://www.youtube.com/watch?v=E9KLxtL1rxs
+
+
+var all = ["NIKOLA TESLA", "ALEJANDRO GONZÁLEZ IÑÁRRITU", "RYAN REYNOLDS", "LUPITA NYONGO", "TED BUNDY","THE UNIBOMBER","CASEY ANTHONY","JEFFREY DAHMER"];
+var classI = ["NIKOLA TESLA", "ALEJANDRO GONZÁLEZ IÑÁRRITU", "RYAN REYNOLDS", "LUPITA NYONGO"];
+var classA = ["TED BUNDY","THE UNIBOMBER","CASEY ANTHONY","JEFFREY DAHMER"];
+var random = '';
+
+
+function nextQuestion(){
+    random = all[(Math.floor(Math.random() * 8))];
+    console.log(random);
+
+    $("#name").empty();
+    $("#name").append(random);
+    return random;
+};
+
+if(random == ''){
+    nextQuestion();
+}
+
+$("#immigrant").click(function(){
+     if(classI.indexOf(random) > -1){
+        $("#answer").empty();
+        $("#answer").append("CORRECT!");
+    }
+    else{
+        $("#answer").empty();
+        $("#answer").append("WRONG!!");
+    }
+
+});
+
+$("#immigrant").click(function(){
+    
+    if(classA.indexOf(random) > -1){
+        $("#answer").empty();
+        $("#answer").append("CORRECT!");
+    }
+    else{
+        $("#answer").empty();
+        $("#answer").append("WRONG!!");
+    }
+});
+
+
+
